5013e7a7058c54547991647362e52c5e829a373b
h-latta <h.latta@yahoo.com>
2022-03-11 13:39:16 -0600

    Imported Bootstrap to Project, Added Styling to MusicForm JSX File

--- src/Components/MusicForm/MusicForm.jsx
+++ src/Components/MusicForm/MusicForm.jsx
@@ -22,20 +22,20 @@ const MusicForm = (props) => {
 
     return ( 
         <form onSubmit={handleSubmit}>
-            <div>
+            <div className="form-group">
                 <h2>Add a song!</h2>
                     <label>Title</label>
-                        <input type='text' value={title} onChange={(event) => setTitle(event.target.value)} />
+                        <input type='text' className="form-control" value={title} onChange={(event) => setTitle(event.target.value)} />
                     <label>Artist</label>
-                        <input type='text' value={artist} onChange={(event) => setArtist(event.target.value)} />
+                        <input type='text' className='form-control' value={artist} onChange={(event) => setArtist(event.target.value)} />
                     <label>Album</label>
-                        <input type='text' value={album} onChange={(event) => setAlbum(event.target.value)} />
+                        <input type='text' className='form-control' value={album} onChange={(event) => setAlbum(event.target.value)} />
                     <label>Release Date</label>
-                        <input type='date' value={date} onChange={(event) => setDate(event.target.value)} />
+                        <input type='date' className='form-control' value={date} onChange={(event) => setDate(event.target.value)} />
                     <label>Genre</label>
-                        <input type='text' value={genre} onChange={(event) => setGenre(event.target.value)} />
+                        <input type='text' className='form-control' value={genre} onChange={(event) => setGenre(event.target.value)} />
             </div>
-            <button type='submit' />
+            <button type='submit' className='btn btn-primary'>SEND</button>
         </form>
      );
 }
